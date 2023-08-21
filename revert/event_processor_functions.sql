c3c0b4054ae59ba6a5e4533f1871e1d31c90579b
Marc-Antoine Parent <maparent@acm.org>
2023-08-21 10:40:09 -0400

    revert event_processor policies

--- revert/event_processor_functions.sql
+++ revert/event_processor_functions.sql
@@ -3,4 +3,10 @@
 
 BEGIN;
 
+DROP POLICY IF EXISTS event_processor_update_policy ON public.event_processor;
+DROP POLICY IF EXISTS event_processor_delete_policy ON public.event_processor;
+DROP POLICY IF EXISTS event_processor_insert_policy ON public.event_processor;
+DROP POLICY IF EXISTS event_processor_select_policy ON public.event_processor;
+ALTER TABLE public.event_processor DISABLE ROW LEVEL SECURITY;
+
 COMMIT;
